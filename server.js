66463b04f12834e6cfd7661fbe1bac150ab51d90
Julianne <juliaguas17@gmail.com>
2023-03-02 22:21:02 -0600

    added POST method

--- server.js
+++ server.js
@@ -18,17 +18,43 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Fetch data
+// GET method for fetching data
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db', 'utf-8', (err, data) => {
+    fs.readFile('./db/db', 'utf8', (err, data) => {
       if (err) {
         console.log(err);
         res.json(err);
       } else {
+        //console.log(data);
         res.send(data);
       }
     })
   });
+
+// POST method
+app.post('/notes', (req, res) => {
+    fs.readFile('./db/db', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.json(err);
+      } else {
+        var newNote = req.body;
+        var parsedNote = JSON.parse(data); 
+        parsedNote.push(newNote);
+  
+        fs.writeFile('./db/db.json', JSON.stringify(parsedNote), (err) => {
+          if (err) {
+            console.log(err);
+            res.json(err);
+          } else {
+            res.status(200);
+            document.location.reload();
+          }
+        })
+      }
+    })
+  });
+  
 
 
 app.listen(PORT, () => {
