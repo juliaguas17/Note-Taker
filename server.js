4d2850b1cee0117edabc30a7d149df00502615cc
Julianne <juliaguas17@gmail.com>
2023-03-02 22:18:04 -0600

    added fetch function to server file

--- server.js
+++ server.js
@@ -6,17 +6,30 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const apiRoutes = require('Note-Taker\routes\apiRoutes');
-const htmlRoutes = require('Note-Taker\routes\htmlRoutes');
-
-app.use(express.urlencoded({
-    extended: true
-}));
-
-app.use(express.static('public'));
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Fetch data
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db', 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.json(err);
+      } else {
+        res.send(data);
+      }
+    })
+  });
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}!`);
